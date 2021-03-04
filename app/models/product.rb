adde5f848654912c85d552dc0740bba5afcb0a51
omani <olehhhm@gmail.com>
2021-03-04 10:16:50 +0200

    fix models

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,8 @@ class Product < ApplicationRecord
   has_many :address, dependent: :destroy
   validates_associated :address
   
-  validates :name, presence: true
-  validates :name, length: { minimum: 3 }
+  validates :name, presence: true, length: { minimum: 3 }
   validates :description, length: { minimum: 3 }
-  validates :is_private, inclusion: [true, false]
-  validates :is_private, exclusion: [nil]
   validates :file1, presence: true
 
   mount_uploader :file1, FileUploader
